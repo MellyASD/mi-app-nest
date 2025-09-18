621e3e6589317ad0697b3b69e52d7fea4dc67032
MellyASD <melissaramirezortegon@hotmail.com>
2025-09-17 20:39:24 -0500

    Nest -- CRUD parte 1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,16 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  //Endpoint de ruta localhost:3000/ que me responde un Hola Mundo
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  //Endpoint de ruta localhost:3000/status que me responde el estado de mi app
+  @Get('status')
+  getStatus() {
+    return this.appService.getStatus();
+  }
 }
+//en el controlador van las rutas y en el services va la logica, las funciones\
