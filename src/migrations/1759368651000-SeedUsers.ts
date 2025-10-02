fa3dcbd2c22bf34db3e3e5e559f0cf21d680917f
MellyASD <melissaramirezortegon@hotmail.com>
2025-10-02 15:44:21 -0500

    actualizacion migraciones

---
+++ src/migrations/1759368651000-SeedUsers.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class SeedUsers1759368651000 implements MigrationInterface {
+    name = 'SeedUsers1759368651000'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+      INSERT INTO user (name, email, password, age) VALUES
+      ('Ana López', 'ana@example.com', '12345', 25),
+      ('Luis Gómez', 'luis@example.com', '12345', 30)
+    `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+                  DELETE FROM user WHERE email IN ('ana@example.com', 'luis@example.com')
+            `)
+    }
+}
