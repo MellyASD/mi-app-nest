3c911d7f2d2565d9005a2762974be3af438b8d8c
MellyASD <melissaramirezortegon@hotmail.com>
2025-10-02 20:35:12 -0500

    clase 2 de octubre y tokens

---
+++ src/migrations/1759447496726-SeedProducts.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class SeedProducts1759447496726 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
