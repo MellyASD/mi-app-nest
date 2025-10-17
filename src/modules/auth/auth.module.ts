adedf4911049be8e63b374188c0b08f2654141b7
MellyASD <melissaramirezortegon@hotmail.com>
2025-10-17 15:30:37 -0500

    error en auth corregido

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,26 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
 import { User } from 'src/entities/user.entity';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt.strategy';
+import { SignOptions } from 'jsonwebtoken';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        secret: config.get<string>('JWT_SECRET_KEY'),
-        signOptions: { expiresIn: config.get<number>('JWT_EXPIRES_IN') ?? 3600 }
-      })
-    })
+ JwtModule.registerAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (config: ConfigService): JwtModuleOptions => ({
+    secret: config.get<string>('JWT_SECRET_KEY'),
+    signOptions: {
+      expiresIn: String(config.get<string>('JWT_EXPIRES_IN') || '45m')
+    } as SignOptions,
+  })
+})
+
+
   ],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController]
